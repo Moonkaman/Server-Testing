aa604a927c47eae1013ba285dbc1f700dcd53cf2
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-07 15:38:44 -0700

    Add router to server.js and complete first dog endpoint

---
+++ api/dogsRouter.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const db = require('../data/helpers/dogsDbHelpers');
+
+router.get('/', (req, res) => {
+  db.get()
+    .then(dogs => res.status(200).json(dogs))
+    .catch(err => res.status(500).json({message: 'Could not retrieve list of dogs at this time', err}))
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const configMiddleware = require('../config/configMiddleware');
 
+const dogsRouter = require('./dogsRouter');
+
 const server = express();
 
 configMiddleware(server);
+server.use('/api/dogs', dogsRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({message: 'It working so far son'});
