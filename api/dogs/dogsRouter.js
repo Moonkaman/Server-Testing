64f3335adb95e68f69e3a3bdae66517554810cc9
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-07 15:41:33 -0700

    Add second dog endpoint

--- api/dogs/dogsRouter.js
+++ api/dogs/dogsRouter.js
@@ -5,7 +5,13 @@ const db = require('../../data/helpers/dogsDbHelpers');
 router.get('/', (req, res) => {
   db.get()
     .then(dogs => res.status(200).json(dogs))
-    .catch(err => res.status(500).json({message: 'Could not retrieve list of dogs at this time', err}))
+    .catch(err => res.status(500).json({message: 'Could not retrieve list of dogs at this time', err}));
+})
+
+router.get('/:id', (req, res) => {
+  db.get(req.params.id)
+    .then(dog => res.status(200).json(dog))
+    .catch(err => res.status(500).json({message: 'Could not retrieve specified dog at this time', err}));
 })
 
 module.exports = router;
