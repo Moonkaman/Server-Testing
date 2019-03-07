c1c2f7a7dde81d594721fbb35f35f94f6a15e642
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-07 15:46:27 -0700

    Added post endpoint to dogs

--- api/dogs/dogsRouter.js
+++ api/dogs/dogsRouter.js
@@ -14,4 +14,16 @@ router.get('/:id', (req, res) => {
     .catch(err => res.status(500).json({message: 'Could not retrieve specified dog at this time', err}));
 })
 
+router.post('/', (req, res) => {
+  if(req.body.name) {
+    req.body.age = req.body.age ? req.body.age : 0;
+    req.body.breed = req.body.breed ? req.body.breed : 'Unknown'
+    db.add(req.body)
+      .then(newDog => res.status(201).json(newDog))
+      .catch(err => res.status(500).json({message: 'Could not create a new dog at this time', err}));
+  } else {
+    res.status(400).json({message: 'Please provide a name for the new dog'});
+  }
+})
+
 module.exports = router;
